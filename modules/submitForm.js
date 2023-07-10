a9705e1a399060595e19fe7e9b41f169df670ce8
wineshuga <uzochukwuwinnie@gmail.com>
2023-07-10 13:55:45 +0100

    Create module submitForm.js

---
+++ modules/submitForm.js
@@ -0,0 +1,17 @@
+import BookList from './classes.js';
+
+const bookList = new BookList();
+
+export default function submitForm(event) {
+  const inputTitle = document.querySelector('.title');
+  const inputAuthor = document.querySelector('.author');
+  event.preventDefault();
+  if (inputTitle.value && inputAuthor.value) {
+    bookList.addBook(inputTitle.value, inputAuthor.value);
+    inputTitle.value = '';
+    inputAuthor.value = '';
+  } else {
+    inputTitle.value = '';
+    inputAuthor.value = '';
+  }
+}
